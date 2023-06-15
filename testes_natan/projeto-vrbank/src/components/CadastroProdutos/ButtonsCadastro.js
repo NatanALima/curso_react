d903ee394e3dccf399a8cbb46058b6f60fed5f3a
Natan Lima <99678150+NatanALima@users.noreply.github.com>
2023-06-15 20:00:03 -0300

    Deleção do projeto vr

--- testes_natan/projeto-vrbank/src/components/CadastroProdutos/ButtonsCadastro.js
+++
@@ -1,9 +0,0 @@
-export default function ButtonsCadastro({btnStyles, handleDelbox, handleAddbox}) {
-    return(
-        <div className={btnStyles.botoes}>
-            <button type="submit" className={`${btnStyles.btn} ${btnStyles.envia}`}> <ion-icon name="checkmark-done-outline"></ion-icon> </button>
-            <button type="button" className={`${btnStyles.btn} ${btnStyles.remove}`} onClick={handleDelbox}> <ion-icon name="close-outline"></ion-icon> </button>
-            <button type="button" className={`${btnStyles.btn} ${btnStyles.add}`} onClick={handleAddbox}> <ion-icon name="add-outline"></ion-icon> </button>
-        </div>
-    )
-}
